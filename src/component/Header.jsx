cf31edd01c401899e3ffa1088ee11eac5da00e14
Alejo020222 <ale75342mb@gmail.com>
2023-08-03 16:19:25 -0400

    first commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    
+return(
+
+
+    <h1 className="text-center font-black text-5xl md:w-2/3 mx-auto ">
+        Seguimiento Pacientes
+        <span className=" text-indigo-700">Veterinaria</span>
+    </h1>
+
+
+
+)
+}
+
+export default Header
